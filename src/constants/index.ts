bafa57416495c6db206295662581f6a2b0b221c0
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-30 19:37:13 +0200

    Fix: TS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -246,7 +246,7 @@ export const myProjects = [
   },
 ];
 
-export const calculateSizes = (isSmall, isMobile, isTablet) => {
+export const calculateSizes = (isSmall: boolean, isMobile: boolean, isTablet: boolean) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
