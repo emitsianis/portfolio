85f962d9691eaa1add3599292a5f09a55f3bf86f
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-30 22:30:03 +0200

    Wip: Hero seciton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from './layout/Navbar.tsx';
+import Hero from './layout/Hero.tsx';
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <Hero />
     </main>
   );
 };
