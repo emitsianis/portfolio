8f27e33e604035491b9e27007dc30de9e8aedda2
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-12-03 20:12:53 +0200

    Mod: Branding

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
-          <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Emitsianis</a>
+          <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Emits</a>
           <button
             onClick={toggleMenu}
             className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
