8bc72de5f8e9e58248e720d5d2dc30fc54bae067
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-12-19 17:13:31 +0200

    Add: Work section id

--- src/layout/Experience.tsx
+++ src/layout/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = () => {
   const [animationName, setAnimationName] = useState('idle');
 
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="work">
       <div className="w-full text-white-600">
         <h3 className="head-text">My work experience</h3>
         <div className="work-container">
